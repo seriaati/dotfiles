e5c02c5cd39e00fdee086defe42c78e72c9f9b6b
seria <seria.ati@gmail.com>
2026-02-27 15:42:36 +0900

    Add WezTerm config

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,17 @@
+    local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.background = {
+    {
+        source = { File = '/home/seria/Pictures/agmmm.jpg' },
+        opacity = 1.0,
+        hsb = { brightness = 0.06,  saturation = 1.0},
+    }
+}
+
+config.window_background_opacity = 1.0
+config.text_background_opacity = 1.0
+config.initial_cols = 100
+config.initial_rows = 24
+
+return config
